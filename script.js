4c5d1cb2ef7617e7c1eef50732515fe99ed348b0
777dan <danya.nikitin7@gmail.com>
2023-09-23 12:59:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// асоціативний масив слів та їх перекладів
+const words = {
+    apple: "яблуко",
+    house: "будинок",
+    mouse: "миша",
+};
+
+let mistakes = 0;
+let correctAnsws = 0;
+let n = +prompt("How many?");
+loop1:
+for (let i = 0; i < n; i++) {
+    for (const key in words) {
+        const element = words[key];
+        //alert(key);
+        let input = prompt("Input translation of " + key);
+        if (input == "" || input == "!") {
+            alert("!!!");
+            break loop1;
+        }
+        if (input === words[key]) {
+            alert("!");
+            correctAnsws++;
+        } else {
+            alert("?");
+            mistakes++;
+        }
+        alert(key + " " + words[key]);
+    }
+}
+alert(correctAnsws + " " + mistakes);
