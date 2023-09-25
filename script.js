188b756be45129f5ef3e45fe26c5ead8d5ef3256
777dan <danya.nikitin7@gmail.com>
2023-09-25 14:29:51 +0300

    added new abilities

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
-// асоціативний масив слів та їх перекладів
-const words = {
-    apple: "яблуко",
-    house: "будинок",
-    mouse: "миша",
+let words = {
+    "apple": "яблуко",
+    "house": "будинок",
+    "mouse": "миша",
 };
 
-let mistakes = 0;
-let correctAnsws = 0;
-let n = +prompt("How many?");
-loop1:
-for (let i = 0; i < n; i++) {
-    for (const key in words) {
-        const element = words[key];
-        //alert(key);
-        let input = prompt("Input translation of " + key);
-        if (input == "" || input == "!") {
-            alert("!!!");
-            break loop1;
+let statistics = {};
+
+let isWordsReversed = false;
+
+function play() {
+    let mistakes = 0;
+    let correctAnsws = 0;
+    let n = +prompt("How many?");
+    loop1:
+    for (let i = 0; i < n; i++) {
+        for (const key in words) {
+            let input = prompt("Input translation of " + key);
+            if (input == "" || input == "!") {
+                alert("!!!");
+                break loop1;
+            }
+            if (input === words[key]) {
+                alert("!");
+                statistics[key] = input;
+                correctAnsws++;
+            } else {
+                alert("?");
+                statistics[key] = input;
+                mistakes++;
+            }
+            alert(key + " " + words[key]);
         }
-        if (input === words[key]) {
-            alert("!");
-            correctAnsws++;
+    }
+    alert(correctAnsws + " " + mistakes);
+    for (const key in statistics) {
+        let cssClass = "";
+        if (statistics[key] === words[key]) {
+            cssClass = "correct";
         } else {
-            alert("?");
-            mistakes++;
+            cssClass = "incorrect";
         }
-        alert(key + " " + words[key]);
+        document.getElementById("statistics").insertAdjacentHTML(
+            "beforeend",
+            `<tr class="${cssClass}"><td>${key}</td><td>${statistics[key]}</td><td>${words[key]}</td></tr>`
+        );
     }
 }
-alert(correctAnsws + " " + mistakes);
+
+function enUa() {
+    if (isWordsReversed) {
+        words = reverseWords(words);
+        isWordsReversed = false;
+    }
+    play();
+}
+
+function uaEn() {
+    words = reverseWords(words);
+    isWordsReversed = true;
+    play();
+}
+
+function reverseWords(words) {
+    const reversedWords = {};
+    for (const key in words) {
+        reversedWords[words[key]] = key;
+    }
+    return reversedWords;
+}
+
+// function collectStatistics() {
+
+// }
